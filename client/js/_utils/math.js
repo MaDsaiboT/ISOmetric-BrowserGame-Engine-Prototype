47f66e953ade27f409f536e5764fa0f324b4fbfa
MaDsaiboT <mad.saibot.dev@gmail.com>
2022-08-01 18:41:06 +0200

    cleanup and shorten functions

--- client/js/_utils/math.js
+++ client/js/_utils/math.js
@@ -2,34 +2,36 @@ const math = {
   radsToDegs: rad => (rad * 180 / Math.PI),
   degsToRads: deg => (deg * Math.PI) / 180.0,
 
-  rand_range: function (a, b) {
-    return Math.random() * (b - a) + a;
-  },
 
-  rand_normalish: function () {
+  rand_range: (a, b) => Math.random() * (b - a) + a,
+
+  rand_normalish: () => {
     const r = Math.random() + Math.random() + Math.random() + Math.random();
     return (r / 4.0) * 2.0 - 1;
   },
 
-  rand_int: function(a, b) {
-    return Math.round(Math.random() * (b - a) + a);
-  },
+  rand_int: (a, b) => Math.round(Math.random() * (b - a) + a),
 
-  lerp: function (x, a, b) {
-    return x * (b - a) + a;
-  },
 
-  clamp: function (x, a, b) {
-    return Math.min(Math.max(x, a), b);
-  },
 
-  sat: function (x) {
-    return Math.min(Math.max(x, 0.0), 1.0);
-  },
+  clamp: (x, a, b) => Math.min(Math.max(x, a), b),
+
+  sat: (x) => Math.min(Math.max(x, 0.0), 1.0),
 
   // normalize x to a value of 0 to 1 in relation to min and max
-  normalize:(x,min,max) => (x - min) / (max - min) 
+  normalize:(x,min,max) => (x - min) / (max - min), 
   
+  //tweening functions
+
+
+  lerp: (x,a,b) =>  x * (b - a) + a,
+
+  // t time normalized
+  // s start
+  // c change 
+  easeInQuad: (t,s,c) => c * t * t + s,
+
+  easeOutQuad: (t,b,s) => -c*t*(t-2) +s,
 };
 
 export default math
