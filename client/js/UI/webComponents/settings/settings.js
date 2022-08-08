60f652e344e692d3f1a6fd23702301183d6982f0
MaDsaiboT <dev.mad.saibot@gmail.com>
2022-08-09 00:10:53 +0200

    rudimentary implementation of (user)settings webComponent

---
+++ client/js/UI/webComponents/settings/settings.js
@@ -0,0 +1,25 @@
+export default class componentSettings extends HTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({mode:'open'});
+  }
+
+  async connectedCallback() {
+    const shadowRoot = this.shadowRoot || this.attachShadow({mode:"open"})
+    //console.log('componentSettings','connected')
+    const src = `${location.origin}/js/UI/webComponents/settings/settings.html`;
+    shadowRoot.innerHTML = await (await fetch(src)).text()
+
+    const form = shadowRoot.querySelector('form');
+    //console.log(form)
+  }
+}
+
+const tagName = 'component-settings'
+if (customElements.get(tagName) === undefined){
+  //console.log(`define custom component <${tagName}>`);
+  customElements.define(tagName,componentSettings)
+}
+
+
