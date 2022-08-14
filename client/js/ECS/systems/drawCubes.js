ee983cb2df7f435cbefea8d38102795d2c48bf57
MaDsaiboT <dev.mad.saibot@gmail.com>
2022-08-14 06:32:32 +0200

    render indicator below selected entity

--- client/js/ECS/systems/drawCubes.js
+++ client/js/ECS/systems/drawCubes.js
@@ -1,17 +1,20 @@
-"use strict";
+'use strict';
 import ecs from '../ecs.js';
-import map from '../../map.js'
+import map from '../../map.js';
 
-import { ctxInteract, viewPortOffset} from '../../main.js';
+import { ctxInteract, viewPortOffset } from '../../main.js';
 const iMap = new map();
 
 const system_drawCubes = () => {
-  ecs.getActiveEntities()
-    .filter(ent=>ent.has('position'))
+  ecs
+    .getActiveEntities()
+    .filter(ent => ent.has('position'))
     .forEach(entity => {
-      iMap.drawCaracter(entity.position,70,ctxInteract,viewPortOffset);
-    })
-}
+      if (entity.tags.has('selected'))
+        iMap.drawTile(entity.position, 70, ctxInteract, viewPortOffset);
 
+      iMap.drawCaracter(entity.position, 70, ctxInteract, viewPortOffset);
+    });
+};
 
-export default system_drawCubes
+export default system_drawCubes;
